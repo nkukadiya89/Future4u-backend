5bcaa0892e8cf5627f42f410e9e8fa6fff476358
Deep Patni <deep.nivzen@gmail.com>
2026-05-13 12:53:29 +0530

    Migration Bug Fixed

--- user_profile/migrations/0038_alter_parentprofile_updated_at_and_more.py
+++ user_profile/migrations/0038_alter_parentprofile_updated_at_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user_profile', '0037_merge_20260505_1152'),
+        ('user_profile', '0037_merge_20260505_1233'),
     ]
 
     operations = [
