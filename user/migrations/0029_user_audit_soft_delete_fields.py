150f22212bfc8ccbacf35df9c2b38953a4b0b962
Deep Patni <deep.nivzen@gmail.com>
2026-06-29 11:05:05 +0530

    user migration bug fixed

--- user/migrations/0029_user_audit_soft_delete_fields.py
+++ user/migrations/0029_user_audit_soft_delete_fields.py
@@ -15,49 +15,40 @@ class Migration(migrations.Migration):
             name="deleted",
             field=models.BooleanField(default=False),
         ),
-        migrations.SeparateDatabaseAndState(
-            state_operations=[
-                migrations.AddField(
-                    model_name="user",
-                    name="deleted_at",
-                    field=models.DateTimeField(blank=True, null=True),
-                ),
-                migrations.AddField(
-                    model_name="user",
-                    name="deleted_by",
-                    field=models.ForeignKey(
-                        blank=True,
-                        null=True,
-                        on_delete=django.db.models.deletion.SET_NULL,
-                        related_name="users_deleted",
-                        to=settings.AUTH_USER_MODEL,
-                    ),
-                ),
-                migrations.AddField(
-                    model_name="user",
-                    name="updated_at",
-                    field=models.DateTimeField(blank=True, db_index=True, null=True),
-                ),
-                migrations.AddField(
-                    model_name="user",
-                    name="updated_by",
-                    field=models.ForeignKey(
-                        blank=True,
-                        null=True,
-                        on_delete=django.db.models.deletion.SET_NULL,
-                        related_name="users_updated",
-                        to=settings.AUTH_USER_MODEL,
-                    ),
-                ),
-            ],
-            database_operations=[
-                migrations.RunSQL(
-                    sql=(
-                        'CREATE INDEX IF NOT EXISTS "user_updated_at_3b607ab0" '
-                        'ON "user" ("updated_at");'
-                    ),
-                    reverse_sql=migrations.RunSQL.noop,
-                ),
-            ],
+
+        migrations.AddField(
+            model_name="user",
+            name="deleted_at",
+            field=models.DateTimeField(blank=True, null=True),
+        ),
+
+        migrations.AddField(
+            model_name="user",
+            name="deleted_by",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="users_deleted",
+                to=settings.AUTH_USER_MODEL,
+            ),
+        ),
+
+        migrations.AddField(
+            model_name="user",
+            name="updated_at",
+            field=models.DateTimeField(blank=True, null=True, db_index=True),
+        ),
+
+        migrations.AddField(
+            model_name="user",
+            name="updated_by",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="users_updated",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
